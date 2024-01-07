bac8c7e48684e3637c9fc8dbd6510492cdb10f5f
Ismael Trocha <ismael.trocha@hotmail.com>
2024-01-07 17:25:07 -0500

    Configuration

--- pqrs-microservice-images/src/main/java/com/pqrs/images/model/Evidence.java
+++ pqrs-microservice-images/src/main/java/com/pqrs/images/model/Evidence.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
-public class MultipartUpload {
+public class Evidence {
 
   private Long id;
   private Long requestId;
--- pqrs-microservice-images/src/test/java/com/pqrs/images/PqrsMicroserviceImagesApplicationTests.java
+++ pqrs-microservice-images/src/test/java/com/pqrs/images/PqrsMicroserviceImagesApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PqrsMicroserviceImagesApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
